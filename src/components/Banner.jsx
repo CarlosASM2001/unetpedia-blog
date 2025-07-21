d031600a9a6596cca0ccd3900256994bc5bcf93d
Carlos Serrano <calsemo2001@gmail.com>
2025-07-21 13:37:33 -0400

    Add .gitignore, update banner images, and modify Navbar styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-
 function Banner() {
   return (
-    <div className="relative m-3 py-40 px-4 sm:px-6 lg:px-8 text-white
-      bg-[url(src/assets/banner7.jpg)] bg-local bg-cover bg-center rounded-2xl"
+    <div
+      className="relative m-3 py-40 px-4 sm:px-6 lg:px-8 text-white bg-local bg-center rounded-2xl"
       style={{
         backgroundSize: "cover",
-        backgroundImage: "linear-gradient(100deg,rgba(30, 58, 138, 0.8) 0%, rgba(30, 58, 138, 0.8) 49%, rgba(30, 58, 138, 0.8) 100%)"
+        backgroundImage: `
+          linear-gradient(90deg, rgba(19,26,26,0.8) 0%, rgba(74,77,74,0.8) 50%, rgba(19,26,26,0.8) 100%),
+          url('/src/assets/banner.png')
+        `
       }}
     >
-      <div className="container mx-auto px-4 text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">Step-by-step guide to choosing great font pairs</h1>
+      <div className="container mx-auto text-center">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">Construyendo el futuro del Aprendizaje Academico</h1>
         <p className="text-xl md:text-2xl mb-8 max-w-3xl mx-auto">
-          Discover insights, stories, and knowledge from our community of writers and experts.
+          Unet Pedia es una plataforma digital universitaria que transforma la forma en que se gestiona y comparte el material académico
         </p>
-        <button className="bg-gradient-to-r from-yellow-400 to-yellow-500 hover:from-yellow-500 hover:to-yellow-600
-          py-3 px-8 rounded-md text-black font-semibold transition-all duration-300 transform hover:scale-105">
-          Read More
-        </button>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import {Menu, X} from 'lucide-react';
-import logo from "../assets/logo-navbar.png";
+import logo from "../assets/Logounetpedia.png";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -12,30 +12,29 @@ function Navbar() {
     }
 
   return (
-    <nav className="sticky top-0 z-50 bg-white shadow-lg">
+    <nav className="sticky top-0 z-50 bg-black shadow-lg">
         <div className="container px-4 mx-auto relative text-[14px]">
             <div className="flex justify-between items-center py-4">
                 <div className="flex items-center flex-shrink-0">
-                    <img src={logo} alt="Logo" className="h-10 w-10 mr-2" />
-                    <span className="text-xl tracking-tight text-black font-semibold">Finvest</span>
+                    <img src={logo} alt="Logo" className="h-15 w-30 mr-2" />
                 </div>
                 <ul className="hidden lg:flex ml-auto space-x-12">
-                    <li><Link to="/" className="text-black hover:text-blue-900 font-medium">Home</Link></li>
-                    <li><Link to="/about" className="text-black hover:text-blue-900 font-medium">About Us</Link></li>
+                    <li><Link to="/" className="text-white hover:text-blue-900 font-medium">Home</Link></li>
+                    <li><Link to="/about" className="text-white hover:text-blue-900 font-medium">About Us</Link></li>
                 </ul>
                 <div className="lg:hidden md:flex flex-col justify-end">
                     <button 
                         onClick={toggleMobileDrawer} 
-                        className="text-black hover:text-blue-900 focus:outline-none">
+                        className="text-white hover:text-blue-900 focus:outline-none">
                         {mobileDrawerOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
                     </button>
                 </div>
             </div>
             {mobileDrawerOpen && (
-                <div className="absolute top-16 left-0 w-full bg-white shadow-lg z-50 border border-gray-200/50">
+                <div className="absolute top-16 left-0 w-full bg-black shadow-lg z-50 border border-gray-200/50">
                     <ul className="flex flex-col items-center space-y-4 py-4">
-                        <li><Link to="/" className="text-black hover:text-blue-900 font-medium">Home</Link></li>
-                        <li><Link to="/about" className="text-black hover:text-blue-900 font-medium">About Us</Link></li>
+                        <li><Link to="/" className="text-white hover:text-blue-900 font-medium">Home</Link></li>
+                        <li><Link to="/about" className="text-white hover:text-blue-900 font-medium">About Us</Link></li>
                     </ul>
                 </div>
             )}
